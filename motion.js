b1e8b41b091ee95206d40dc290fdc44c3a546125
Gabriel S Vieira <128159150+Gabriel-S-Vieira@users.noreply.github.com>
2023-05-25 19:18:21 -0300

    Add files via upload

---
+++ motion.js
@@ -0,0 +1,7 @@
+var coisas = document.getElementById('coisas')
+var cookies = document.getElementsByClassName('cookies')
+var butoes = document.getElementById('but')
+
+butoes.addEventListener('click', () => {
+    coisas.style = 'display: none'
+})
